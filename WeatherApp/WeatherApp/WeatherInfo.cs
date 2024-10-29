4fe0b08d805ba0d40e2746f5b4ef048f92b261a4
Nguyen-Trung-Tien <2251120447@ut.edu.vn>
2024-10-29 21:14:41 +0700

    Fix WeatherInfo

--- WeatherApp/WeatherApp/WeatherInfo.cs
+++ WeatherApp/WeatherApp/WeatherInfo.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
 
-namespace Weather
+namespace Weather_Application
 {
     class WeatherInfo
     {
@@ -21,6 +23,13 @@ namespace Weather
             public string icon { get; set; }
 
         }
+
+        public class GeoLocation
+        {
+            public string name { get; set; }
+            public string country { get; set; }
+        }
+
         public class main
         {
             public double temp { get; set; }
@@ -29,11 +38,11 @@ namespace Weather
             public double humidity { get; set; }
             public double temp_min { get; set; }
             public double temp_max { get; set; }
+            public double sea_level { get; set; }
+            public double grnd_level { get; set; }
         }
 
-
-        
-        public class wind
+        public class Wind
         {
             public double speed { get; set; }
             public double deg { get; set; }
@@ -41,20 +50,40 @@ namespace Weather
 
         public class sys
         {
+            public string name { get; set; }
             public long sunrise { get; set; }
             public long sunset { get; set; }
-            public long microrise { get; set; }
-
-
+            public string country { get; set; }
         }
+
+        public class clouds
+        {
+            public int all { get; set; }
+        }
+        public class Forecast
+        {
+            public long dt { get; set; }
+            public main main { get; set; }
+            public List<weather> weather { get; set; }
+            public Wind wind { get; set; }
+            public clouds clouds { get; set; }
+        }
+
+        public class ForecastRoot
+        {
+            public List<Forecast> list { get; set; }
+        }
+
 
         public class root
         {
+            public GeoLocation geoLocation { get; set; }
             public coord coord { get; set; }
             public List<weather> weather { get; set; }
             public main main { get; set; }
-            public wind wind { get; set; }
+            public Wind wind { get; set; }
             public sys sys { get; set; }
+            public clouds clouds { get; set; }
 
         }
 
